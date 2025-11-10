7222490f5f68fe7f1a6aa682a9dd1274dc620217
sneha <86240632+chintalasnehaprabha@users.noreply.github.com>
2025-11-10 12:18:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,24 +6,24 @@ version= "~>3.0"
 }
 }
 }
-provider "azurerm"{
-features{}
+provider "azurerm" {
+features {}
 }
 
 resource "azurerm_resource_group" "rg"{
 name= var.resource_group_name
-resource_group_location= var.location
+location= var.location
 }
 resource "azurerm_virtual_network" "vnet"{
 name=var.azm_vnet_name
-resource_group_name = var.azurerm_resource_group.rg.name
-resource_group_location = var.azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+resource_group_location = azurerm_resource_group.rg.location
 address_space= var.address_space
 }
 resource "azurerm_subnets" "azsub"{
 name=var.az_subnet_name
-resource_group_name=var.azm-resource-group.rg.name
-virtual_network_name=var.azurerm_virtual_network.vnet.name
+resource_group_name=azm-resource-group.rg.name
+virtual_network_name=azurerm_virtual_network.vnet.name
 address_prefixes =var.subnet_prefixes
 }
 }
