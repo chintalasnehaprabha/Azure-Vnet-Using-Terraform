dd5e97977321725a337df27893e66586cea08b5b
sneha <86240632+chintalasnehaprabha@users.noreply.github.com>
2025-11-10 11:58:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+required_providers {
+azurerm= {
+source= "hashicorp/azurerm"
+version= "~>3.0"
+}
+provider "azurerm"{
+features()
+}
+
+resource "azm-resource-group" "rg"{
+name= var.resource_group_name
+location= var.location
+}
+resource "azm-vnet" "aznet"{
+name=var.azm_vnet_name
+resource_group=var.azm-resource-group.rg.name
+resource_group_location= var.azm-resource-group.location
+address_space= var.address_space
+}
+
+
