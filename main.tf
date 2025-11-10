ad45d9aefef7cc52074352a6ec1fbb6e26a717be
sneha <86240632+chintalasnehaprabha@users.noreply.github.com>
2025-11-10 12:07:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,25 +5,26 @@ source= "hashicorp/azurerm"
 version= "~>3.0"
 }
 }
+}
 provider "azurerm"{
 features{}
 }
 
-resource "azm-resource-group" "rg"{
+resource "azurerm_resource_group" "rg"{
 name= var.resource_group_name
 location= var.location
 }
-resource "azm-vnet" "aznet"{
+resource "azurerm_virtual_network" "aznet"{
 name=var.azm_vnet_name
-resource_group=var.azm-resource-group.rg.name
-resource_group_location= var.azm-resource-group.location
+resource_group=var.azurerm_resource_group.rg.name
+resource_group_location= var.azurerm_resource_group.location
 address_space= var.address_space
 }
-resource "azm-subnet" "azsub"{
+resource "azurerm_subnets" "azsub"{
 name=var.az_subnet_name
 resource_group=var.azm-resource-group.rg.name
 az_vnet=var.azm-vnet.aznet.name
-address_prefix=var.subnet_prefix
+address_prefixes =var.subnet_prefixes
 }
 }
 
