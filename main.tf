7cf4db4b0121eaa2611f4d026a55b63f63cc7161
sneha <86240632+chintalasnehaprabha@users.noreply.github.com>
2025-11-10 12:13:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ features{}
 
 resource "azurerm_resource_group" "rg"{
 name= var.resource_group_name
-location= var.location
+resource_group_location= var.location
 }
-resource "azurerm_virtual_network" "azurerm_virtual_network"{
+resource "azurerm_virtual_network" "vnet"{
 name=var.azm_vnet_name
 resource_group_name = var.azurerm_resource_group.rg.name
 resource_group_location = var.azurerm_resource_group.rg.location
@@ -22,8 +22,8 @@ address_space= var.address_space
 }
 resource "azurerm_subnets" "azsub"{
 name=var.az_subnet_name
-resource_group=var.azm-resource-group.rg.name
-az_vnet=var.azm-vnet.aznet.name
+resource_group_name=var.azm-resource-group.rg.name
+virtual_network_name=var.azurerm_virtual_network.vnet.name
 address_prefixes =var.subnet_prefixes
 }
 }
